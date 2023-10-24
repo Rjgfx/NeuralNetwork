b933f450ac40bf6379da8d5433d7abe4d7a23e76
Rjgfx <132141070+Rjgfx@users.noreply.github.com>
2023-10-25 00:38:18 +0300

    Add files via upload

---
+++ Network/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Network
+{
+    internal static class Program
+    {
+        [STAThread]
+
+        static void Main()  
+        {
+
+            int[] sizes_of_layers = new int[] { 784, 16, 16, 10 };
+            NeuralNetwork nn = new NeuralNetwork(sizes_of_layers, ActivationFunctions.sigmoid, ActivationFunctions.dsigmoid, 
+                ActivationFunctions.sigmoid, ActivationFunctions.dsigmoid, 0.1);
+
+            nn.Train();
+            nn.Test();
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+        }
+    }
+}
+
+
